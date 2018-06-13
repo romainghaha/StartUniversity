df29e707ff2309784797f12a9f71cd9b97190e3e
苏强 <suqiang171127@credithc.com>
2018-06-13 17:10:14 +0800

    添加悬浮按钮

--- StartUniversity/BaseViewControllers/SUMainTabBarController.m
+++ StartUniversity/BaseViewControllers/SUMainTabBarController.m
@@ -7,9 +7,10 @@
 //
 
 #import "SUMainTabBarController.h"
+#import <Masonry.h>
 
 @interface SUMainTabBarController ()
-
+@property (nonatomic, strong)UIButton *floatingButton;
 @end
 
 @implementation SUMainTabBarController
@@ -22,16 +23,34 @@
     [self addChildViewControllerWithClassName:@"SUMessageViewController" andImageName:@"" andTitle:@"消息"];
     [self addChildViewControllerWithClassName:@"SUCircleFriendsViewController" andImageName:@"" andTitle:@"朋友圈"];
     [self addChildViewControllerWithClassName:@"SUPersonalCenterViewController" andImageName:@"" andTitle:@"我的"];
+    [self addFloatingButton];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-//    [self.tabBar setHidden:YES];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)addFloatingButton {
+    
+    [self.view addSubview:self.floatingButton];
+    NSLog(@"走了");
+    [self.floatingButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.offset(-80);
+        make.right.offset(-40);
+        make.height.equalTo(@80);
+        make.width.equalTo(@80);
+    }];
+    
+}
+
+- (void)floatingButtonClickAction{
+    NSLog(@"点击了");
 }
 
 - (void)addChildViewControllerWithClassName:(NSString *)className andImageName:(NSString *)imageName andTitle:(NSString *)title {
@@ -46,6 +65,19 @@
     root.tabBarItem.title = title;
     
     [self addChildViewController:root];
+}
+
+- (UIButton *)floatingButton {
+    if (!_floatingButton) {
+        _floatingButton = [[UIButton alloc] init];
+        [_floatingButton setTitle:@"按钮" forState:UIControlStateNormal];
+        [_floatingButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [_floatingButton setBackgroundColor:[UIColor redColor]];
+        [_floatingButton addTarget:self action:@selector(floatingButtonClickAction) forControlEvents:UIControlEventTouchUpInside];
+        _floatingButton.layer.cornerRadius = 40;
+        _floatingButton.layer.masksToBounds = YES;
+    }
+    return _floatingButton;
 }
 
 @end
