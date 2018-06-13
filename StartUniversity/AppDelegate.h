017811862eafc416e59ecd616b2ff70f30ccf51c
苏强 <suqiang171127@credithc.com>
2018-06-13 14:09:53 +0800

    项目初始化

---
+++ StartUniversity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StartUniversity
+//
+//  Created by 苏强 on 2018/6/13.
+//  Copyright © 2018年 苏强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StartUniversity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StartUniversity
+//
+//  Created by 苏强 on 2018/6/13.
+//  Copyright © 2018年 苏强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
